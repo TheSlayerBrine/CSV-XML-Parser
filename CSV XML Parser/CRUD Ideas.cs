bf3d5816863f0fe1d1b64cae0cb18ef9f58eab08
TheSlayerBrine <cgamer788@gmail.com>
2023-07-30 12:57:53 +0300

    Okay for testing?

---
+++ CSV XML Parser/CRUD Ideas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSV_XML_Parser
+{
+    internal class CRUD_Ideas
+    {
+    }
+}
---
+++ Tests/CSVParserTest.cs
@@ -0,0 +1,24 @@
+﻿using CsvXmlParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    
+    public class CSVParserTest
+    {
+        private readonly CSVParser sut;
+        [Fact]
+        public CSVParserTest()
+        {
+            sut= new CSVParser();s
+        }
+        public void SplitDataTest()
+        {
+
+        }
+    }
+}
